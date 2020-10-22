f584eab8a649a8d00b7334ffdc20ef3d8a01046f
Wei He <git@weispot.com>
2020-10-21 23:32:12 -0400

    reproduce aws-amplify/amplify-js#5206

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Amplify, { DataStore } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { Product } from './models';
+Amplify.configure(awsExports);
+
+async function addProduct() {
+  const newProduct = await DataStore.save(
+    new Product({
+      name: `Product_${Math.random()}`,
+      owner: `Owner_${Math.random()}`,
+      imageKey: `Image_${Math.random()}`
+    })
+  )
+
+  return newProduct
+}
+
+async function removeImageFromProduct(id: string) {
+  const product = await DataStore.query(Product, id)
+  if (!product) {
+    alert('Product not found')
+    return null
+  }
+
+  const productCopy = Product.copyOf(product, updated => {
+    updated.imageKey = undefined
+  })
+
+  const updatedProduct = await DataStore.save(
+    productCopy
+  )
+
+  return updatedProduct
+}
+
 function App() {
+  const [product, setProduct] = useState<Product | null>()
+
+  useEffect(() => {
+    (async() => {
+      const p = await addProduct()
+      setProduct(p)
+
+      setTimeout(async () => {
+        const updated_p = await removeImageFromProduct(p.id)
+        setProduct(updated_p)
+      }, 2000)
+    })()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <pre>{JSON.stringify(product, null, 2)}</pre>
         <a
           className="App-link"
           href="https://reactjs.org"
